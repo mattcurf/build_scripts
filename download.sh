cccc42c87d82dbbebf73949b854df4adcbe93fa8
Matt Curfman <matt@tecnovelty.com>
2019-04-02 03:40:15 +0000

    Initial version

---
+++ download.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+git clone git://git.yoctoproject.org/poky sources/poky
+git clone git://git.yoctoproject.org/meta-intel	sources/meta-intel
+git clone git://git.yoctoproject.org/meta-zephyr sources/meta-zephyr
+git clone git://git.openembedded.org/meta-openembedded sources/meta-openembedded
+git clone https://github.com/sergioprado/bitbake-bash-completion sources/bitbake-bash-completion
+git clone https://github.com/mattcurf/meta-acrn sources/meta-acrn
+git clone https://github.com/mattcurf/docker-builder docker
