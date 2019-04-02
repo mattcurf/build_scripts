dde0cc1a2b74e5a545b66d81798256c6b6bac641
Matt Curfman <matt@tecnovelty.com>
2019-04-02 10:40:05 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,9 +1,27 @@
 #!/bin/bash
 
-git clone git://git.yoctoproject.org/poky sources/poky
-git clone git://git.yoctoproject.org/meta-intel	sources/meta-intel
-git clone git://git.yoctoproject.org/meta-zephyr sources/meta-zephyr
-git clone git://git.openembedded.org/meta-openembedded sources/meta-openembedded
-git clone https://github.com/sergioprado/bitbake-bash-completion sources/bitbake-bash-completion
-git clone https://github.com/mattcurf/meta-acrn sources/meta-acrn
-git clone https://github.com/mattcurf/docker-builder docker
+git config --global user.email "builder@user.com"
+git config --global user.name "builder"
+git config --global color.ui false
+
+mkdir -p workspace/celadon
+pushd workspace/celadon
+#repo init -q -u https://github.com/projectceladon/manifest.git
+repo init -q -u file:///mnt/ws/cache/celadon_mirror/manifest.git
+repo sync
+popd
+
+mkdir -p workspace/system
+pushd system
+repo init -q -u https://github.com/mattcurf/meta-acrn-manifest.git -b master
+repo sync
+popd
+
+#./setup -a ../celadon
+#sources/meta-acrn/acrn-init-build-env build
+#bitbake uos-image-weseton
+
+#MACHINE=apl-nuc bitbake sos-image-weston
+#source build/envsetup.sh
+#lunch celadon-userdebug
+#make SPARSE_IMG=true project_celadon-efi -j $(nproc)
