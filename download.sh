9d4b461d04a4ee2b63ce5721b7f9129780163b85
Matt Curfman <matt@tecnovelty.com>
2019-04-02 13:56:03 -0700

    Update download.sh

--- download.sh
+++ download.sh
@@ -1,25 +1,28 @@
 #!/bin/bash
-
+  
 git config --global user.email "builder@user.com"
 git config --global user.name "builder"
 git config --global color.ui false
 
-mkdir -p workspace/celadon
-pushd workspace/celadon
+mkdir -p workspace/celadon && pushd workspace/celadon
 #repo init -q -u https://github.com/projectceladon/manifest.git
-repo init -q -u file:///mnt/ws/cache/celadon_mirror/manifest.git
-repo sync
+repo init -q -u file:///mnt/ws/cache/celadon_mirror/manifest.git -m stable-build/ww201913.xml
+repo sync -j $(nproc)
 popd
 
 mkdir -p workspace/system
-pushd system
+pushd workspace/system
 repo init -q -u https://github.com/mattcurf/meta-acrn-manifest.git -b master
-repo sync
+repo sync -j $(nproc)
 popd
 
-#./setup -a ../celadon
-#sources/meta-acrn/acrn-init-build-env build
-#bitbake uos-image-weseton
+pushd workspace/system
+. acrn-init-build-env
+cat > conf/local.conf << EOF
+EXTERNALSRC = `pwd`../../celadon
+EOF
+#MACHINE=apl-nuc bitbake uos-image-weston
+popd
 
 #MACHINE=apl-nuc bitbake sos-image-weston
 #source build/envsetup.sh
